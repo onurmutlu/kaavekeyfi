e8429724f29a06e7156be4e8117197502795f1f5
Mono Cidar <onurmutlu@gmail.com>
2018-11-25 14:34:50 +0300

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,415 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <link rel="apple-touch-icon" sizes="76x76" href="/assets/img/apple-icon.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/assets/img/favicon.png">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+    <title>KAAVE Keyfi - Yeni Jenerasyon Kahve Deneyimi</title>
+
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width" />
+
+    <link href="/bootstrap3/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="/assets/css/gsdk.css" rel="stylesheet"/>
+    <link href="/assets/css/examples.css" rel="stylesheet"/>
+
+    <!--     Fonts and icons     -->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Grand+Hotel|Open+Sans:400,300' rel='stylesheet' type='text/css'>
+    <link href="/assets/css/pe-icon-7-stroke.css" rel="stylesheet" />
+</head>
+
+<body class="home">
+
+<nav class="navbar navbar-inverse navbar-transparent navbar-fixed-top" role="navigation">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button id="menu-toggle" type="button" class="navbar-toggle">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar bar1"></span>
+                <span class="icon-bar bar2"></span>
+                <span class="icon-bar bar3"></span>
+            </button>
+            <a class="navbar-brand" href="/">Kaave Keyfi</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse">
+            <ul  class="nav navbar-nav navbar-right">
+                <li>
+                    <a href="/about-us">
+                        Hakkımızda
+                    </a>
+                </li>
+                <li>
+                    <a href="/subeler">
+                        Şubeler
+                    </a>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        Menü
+                        <b class="caret"></b>
+                    </a>
+                    <ul class="dropdown-menu dropdown-with-icons">
+                        <li>
+                            <a href="/menu-yiyecek">
+                                <i class="pe-7s-comment"></i> Yiyecekler
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/menu-icecek">
+                                <i class="pe-7s-news-paper"></i> İçecekler
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        Alışveriş
+                        <b class="caret"></b>
+                    </a>
+                    <ul class="dropdown-menu dropdown-with-icons">
+                        <li>
+                            <a href="/shop-kahve">
+                                <i class="pe-7s-ticket"></i> Kahveler
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/shop-makina">
+                                <i class="pe-7s-piggy"></i> Makinalar
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        Eğitimler
+                        <b class="caret"></b>
+                    </a>
+                    <ul class="dropdown-menu dropdown-with-icons">
+                        <li>
+                            <a href="#">
+                                <i class="pe-7s-mail-open-file"></i> Contact Us
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="pe-7s-info"></i> About Us
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="pe-7s-cash"></i> Pricing Page
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="pe-7s-gift"></i> More (soon)
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li><a href="https://kaavekeyfi.com/shop" class="btn btn-round btn-default"><i class="fa fa-coffee"></i> 10</a></li>
+            </ul>
+
+        </div><!-- /.navbar-collapse -->
+    </div>
+</nav>
+
+<div class="wrapper">
+    <div class="parallax filter-black">
+        <div class="parallax-image">
+            <img src="/assets/img/picto/bg18.png" alt="..." />
+        </div>
+        <div class="small-info">
+            <h1>Kaave Keyfi</h1>
+            <h3>New Generation Old Style</h3>
+        </div>
+    </div>
+
+    <div class="section section-description">
+        <div class="container">
+            <h2 class="section-title">Yeni Jenerasyon</h2>
+
+            <div class="row">
+                <div class="col-md-6">
+                    <h5>Üçüncü Dalga Kahve Deneyimi</h5>
+                    <p>
+                        ‘Nedir bu üçüncü dalga kahveciler’ diye soranları, çekingenliklerinden kurtulmaları için davet ediyoruz. </p>
+                    </p>Bu küçük bilgilerle artık mis gibi kahve kokusu burnunuzdan hiç ayrılmayacak. </p>
+
+                </div>
+                <div class="col-md-6">
+                    <div id="description-carousel" class="carousel fade" data-ride="carousel">
+                        <!-- Wrapper for slides -->
+                        <div class="carousel-inner">
+                            <div class="item active">
+                                <img src="/assets/img/picto/bg10.png" alt="..." />
+                            </div>
+                            <div class="item">
+                                <img src="/assets/img/picto/bg14.png" alt="..." />
+                            </div>
+                            <div class="item">
+                                <img src="/assets/img/picto/bg15.png" alt="..." />
+                            </div>
+                            <div class="item">
+                                <img src="/assets/img/picto/bg17.png" alt="..." />
+                            </div>
+                            <div class="item">
+                                <img src="/assets/img/picto/bg22.png" alt="..." />
+                            </div>
+                        </div>
+
+                        <ol class="carousel-indicators">
+                            <li data-target="#description-carousel" data-slide-to="0" class="active"></li>
+                            <li data-target="#description-carousel" data-slide-to="1"></li>
+                            <li data-target="#description-carousel" data-slide-to="2"></li>
+                            <li data-target="#description-carousel" data-slide-to="3"></li>
+                            <li data-target="#description-carousel" data-slide-to="4"></li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+
+            <div class="space-50"></div>
+            <hr>
+            <div class="space-50"></div>
+
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="img-container">
+                        <img src="/assets/img/picto/bg19.png" alt="..." />
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <h5>Üçüncü Dalga Nitelikli Kahve Akımı</h5>
+                    <p>Birinci dalga 1900’lü yılların başında evlere giren dondurulmuş kahve markalarıyla, ikinci dalga ise yakın tarihteki zincir kahvecilerle yani ‘latte’nin, ‘espresso’nun günlük hayatımıza girmesiyle devam ediyor.</p>
+                    <p>Üçüncü dalgada ise çekirdekler, satın alındığı ülkeler yerine, yöresi ve çiftlikleriyle anılıyor. Kavurma işlemi karakteristik özelliklerini ortaya çıkaran bir sanat haline dönüşüyor.</p>
+                    <p>Aromaları ise başımızı döndürüyor.</p>
+
+                </div>
+            </div>
+        </div>
+    </div><!-- section -->
+    <div class="space-50"></div>
+
+
+
+    <div class="social-line">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <a href="" class="btn btn-round btn-fill btn-social btn-facebook">
+                        <i class="fa fa-facebook-square"></i> Facebook
+                    </a>
+                </div>
+                <div class="col-md-3">
+                    <a href="" class="btn btn-round btn-fill btn-social btn-twitter">
+                        <i class="fa fa-twitter"></i> Twitter
+                    </a>
+                </div>
+                <div class="col-md-3">
+                    <a href="" class="btn btn-round btn-fill btn-social btn-google">
+                        <i class="fa fa-google-plus-square"></i> Google+
+                    </a>
+                </div>
+                <div class="col-md-3">
+                    <a href="" class="btn btn-round btn-fill btn-social btn-pinterest">
+                        <i class="fa fa-pinterest"></i> Pinterest
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <footer class="footer footer-big">
+        <!-- .footer-black is another class for the footer, for the transparent version, we recommend you to change the url of the image with your favourite image.          -->
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-2">
+                    <h5 class="title">Company</h5>
+                    <nav>
+                        <ul>
+                            <li>
+                                <a href="#" >
+                                    Home
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Find offers
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Discover Projects
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Our Portfolio
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    About Us
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="col-md-3 col-md-offset-1">
+                    <h5 class="title"> Support</h5>
+                    <nav>
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    Contact Us
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    How it works
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    24/7 Available
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    We are hiring
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Send Info
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="col-md-3">
+                    <h5 class="title"> Help</h5>
+                    <nav>
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    Contact Us
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Terms & Conditions
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Company Policy
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Money Back
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+
+                </div>
+
+                <div class="col-md-3">
+                    <h5 class="title">Latest News</h5>
+                    <nav>
+                        <ul>
+                            <li>
+                                <a href="#" >
+                                    <i class="fa fa-twitter"></i> <b>Get Shit Done</b>
+                                    The best kit in the market is here, just give it a try and let us...
+                                    <hr class="hr-small">
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" >
+                                    <i class="fa fa-twitter"></i>
+                                    We've just been featured on <b> Awwwards Website</b>! Thank you everybody for...
+                                </a>
+                            </li>
+
+                        </ul>
+                    </nav>
+                </div>
+
+            </div>
+            <hr />
+            <div class="copyright">
+                1999 - 2019 &copy; SiyahKare
+            </div>
+        </div>
+    </footer>
+
+
+</div> <!-- wrapper -->
+</body>
+<!--  jQuery and Bootstrap core files    -->
+<script src="/assets/js/jquery.js" type="text/javascript"></script>
+<script src="/assets/js/jquery-ui.custom.min.js" type="text/javascript"></script>
+<script src="/bootstrap3/js/bootstrap.min.js" type="text/javascript"></script>
+
+<!--  Plugins -->
+<script src="/assets/js/gsdk-checkbox.js"></script>
+<script src="/assets/js/gsdk-morphing.js"></script>
+<script src="/assets/js/gsdk-radio.js"></script>
+<script src="/assets/js/gsdk-bootstrapswitch.js"></script>
+<script src="/assets/js/bootstrap-select.js"></script>
+<script src="/assets/js/bootstrap-datepicker.js"></script>
+<script src="/assets/js/chartist.min.js"></script>
+<script src="/assets/js/jquery.tagsinput.js"></script>
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js"></script>
+
+<!--  Get Shit Done Kit PRO Core javascript 	 -->
+<script src="/assets/js/get-shit-done.js"></script>
+
+<script type="text/javascript">
+    var big_image;
+    $().ready(function(){
+        responsive = $(window).width();
+
+        $(window).on('scroll', gsdk.checkScrollForTransparentNavbar);
+
+        if (responsive >= 768){
+            big_image = $('.parallax-image').find('img');
+
+            $(window).on('scroll',function(){
+                parallax();
+            });
+        }
+
+    });
+
+    var parallax = function() {
+        var current_scroll = $(this).scrollTop();
+
+        oVal = ($(window).scrollTop() / 3);
+        big_image.css('top',oVal);
+    };
+
+</script>
+
+<!-- If you are using TypeKit.com uncomment this code otherwise you can delete it -->
+<!--
+<script src="https://use.typekit.net/[your kit code here].js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+-->
+
+<!-- If you have retina @2x images on your server which can be sent to iPhone/iPad/MacRetina, please uncomment the next line, otherwise you can delete it -->
+<!-- <script src="assets/js/retina.min.js"></script> -->
+
+</html>
